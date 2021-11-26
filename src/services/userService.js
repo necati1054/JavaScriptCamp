f65da8bf8728377a01c5a9af8e203b7f1f0a7812
Necati <necati.arman1054@gmail.com>
2021-11-26 21:16:12 +0300

    yarım
    yarım

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,20 +1,70 @@
-export default class UserService{
-    constructor(loggerService){
-        this.users = []
-        this.loggerService =loggerService
+import { users } from "../data/users.js"
+import DataError from "../models/dataError.js"
+import User from "../models/user.js"
+
+export default class UserService {
+    constructor(loggerService) {
+        this.employees = []
+        this.customers = []
+        this.errors = []
+        this.loggerService = loggerService
     }
 
-    add(user){
-        this.users.push(user)
+    load() {
+        for (const user of users) {
+            switch (user.type) {
+                case "customer":
+                    if (!this.checkCustomerValidtyForErrors(User)) {
+                        this.customers.push(user)
+                    }
+                    break;
+                case "employee":
+                    if (!this.checkEmployeeValidtyForErrors(User)) {
+                        this.customers.push(user)
+                    }
+                    this.employees.push(user)
+                    break;
+                default:
+                    this.errors.push(new DataError("Wrong user type", user))
+                    break;
+            }
+        }
+    }
+
+    checkCustomerValidtyForErrors(user) {
+        let requiredFields = "id firstName lastName age city".split("  ")
+        let hasErrors = false
+        for (const field of requiredFields) {
+            if (!user[field]) {
+                hasErrors = true
+                this.errors.push(new DataError(`Validation problem. ${field} is required `, user))
+            }
+        }
+        return hasErrors
+    }
+    checkEmployeeValidtyForErrors(user) {
+        let requiredFields = "id firstName lastName age city salary".split("  ")
+        let hasErrors = false
+        for (const field of requiredFields) {
+            if (!user[field]) {
+                hasErrors = true
+                this.errors.push(new DataError(`Validation problem. ${field} is required `, user))
+            }
+        }
+        return hasErrors
+    }
+
+    add(user) {
+        //this.users.push(user)
         this.loggerService.log(user)
     }
 
-    list(){
-        return this.users
+    list() {
+        //return this.users
     }
 
-    getById(id){
-        return this.users.find(u=>u.id ===id)
+    getById(id) {
+        //return this.users.find(u=>u.id ===id)
     }
 
 }
